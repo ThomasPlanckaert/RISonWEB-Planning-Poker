5f6a51bdf26428e4fa79c6cc516e326e2376f349
t.planckaert <thomas.planckaert@dedalus.com>
2026-05-08 13:17:41 +0200

    Remove base in vite config

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,7 +3,7 @@ import { LandingPage } from "../pages/LandingPage";
 import { SessionPage } from "../pages/SessionPage";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <LandingPage /> },
+  { path: "/*", element: <LandingPage /> },
   { path: "/session/:sessionId", element: <SessionPage /> },
   { path: "/session/:sessionId/admin", element: <SessionPage admin /> }
 ]);
